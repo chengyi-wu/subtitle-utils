4bd5908f5d67fc84f3f302ee4071f4cbedf7ab0d
chenwu <chengyi.wu@outlook.com>
2019-01-21 13:58:36 +0800

    + sbv -> srt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # subtitle-utils
+
+Download youtube sbv file and turn it into srt file. Open the srt in aegisub or PR to make translated video.
---
+++ SBV2SRT.py
@@ -0,0 +1,24 @@
+count = 0
+is_newtimeline = True
+out = open('out.srt', 'w', encoding='utf-8')
+with open('captions (2).sbv', 'r', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip()
+        if len(line) == 0:
+            is_newtimeline = True
+            print(line)
+            out.write(line + '\n')
+        elif is_newtimeline:
+                is_newtimeline = False
+                count += 1
+                print(count)
+                out.write(str(count) + '\n')
+                start, end = line.split(',')
+                start = start.replace('.', ',')
+                end = start.replace('.', ',')
+                print("%s --> %s" % (start, end))
+                out.write("%s --> %s\n" % (start, end))
+        else:
+            print(line)
+            out.write(line + '\n')
+out.close()
