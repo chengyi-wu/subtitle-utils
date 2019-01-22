6673fe17c27248785605de4a8744c0ea098f5286
chenwu <chengyi.wu@outlook.com>
2019-01-22 16:19:29 +0800

    Update SBV2SRT.py
    + make it into functions

--- SBV2SRT.py
+++ SBV2SRT.py
@@ -1,24 +1,31 @@
-count = 0
-is_newtimeline = True
-out = open('out.srt', 'w', encoding='utf-8')
-with open('captions(1).sbv', 'r', encoding='utf-8') as f:
-    for line in f:
-        line = line.strip()
-        if len(line) == 0:
-            is_newtimeline = True
-            print(line)
-            out.write(line + '\n')
-        elif is_newtimeline:
-                is_newtimeline = False
-                count += 1
-                print(count)
-                out.write(str(count) + '\n')
-                start, end = line.split(',')
-                start = start.replace('.', ',')
-                end = end.replace('.', ',')
-                print("%s --> %s" % (start, end))
-                out.write("%s --> %s\n" % (start, end))
-        else:
-            print(line)
-            out.write(line + '\n')
-out.close()
+import sys
+
+def convert(src, dst='out.srt'):
+    count = 0
+    is_newtimeline = True
+    out = open(dst, 'w', encoding='utf-8')
+    with open(src, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                is_newtimeline = True
+                print(line)
+                out.write(line + '\n')
+            elif is_newtimeline:
+                    is_newtimeline = False
+                    count += 1
+                    print(count)
+                    out.write(str(count) + '\n')
+                    start, end = line.split(',')
+                    start = start.replace('.', ',')
+                    end = end.replace('.', ',')
+                    print("%s --> %s" % (start, end))
+                    out.write("%s --> %s\n" % (start, end))
+            else:
+                print(line)
+                out.write(line + '\n')
+    out.close()
+
+if __name__ == '__main__':
+    src = sys.argv[1]
+    convert(src)
